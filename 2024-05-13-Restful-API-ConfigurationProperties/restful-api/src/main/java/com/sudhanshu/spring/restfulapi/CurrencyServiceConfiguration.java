b0ac3d1c0aec23bda902336d40325cc3358683de
Sudhanshu Yadav <117539682+sudhanshu-y@users.noreply.github.com>
2024-05-13 19:10:00 +0530

    Spring ConfigurationProperties

---
+++ 2024-05-13-Restful-API-ConfigurationProperties/restful-api/src/main/java/com/sudhanshu/spring/restfulapi/CurrencyServiceConfiguration.java
@@ -0,0 +1,43 @@
+package com.sudhanshu.spring.restfulapi;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "currency-service")
+@Component
+public class CurrencyServiceConfiguration {
+
+	private String url;
+	private String user;
+	private String key;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	@Override
+	public String toString() {
+		return "CurrencyController [url=" + url + ", user=" + user + ", key=" + key + "]";
+	}
+
+}
